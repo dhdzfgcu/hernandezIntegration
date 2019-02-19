44ec9d2cc90d67c584633ca5df79391f956311cb
dahernandez0094 <dahernandez0094@H202R1S1.STUDENT.ad.fgcu.edu>
2019-02-19 16:16:40 -0500

    letsgo

--- hernandezIntegration/src/Main.java
+++ hernandezIntegration/src/Main.java
@@ -9,30 +9,50 @@ public class Main {
   // public is an access modifier
   // void is the return type
   public static void main(String[] args) {
+    Scanner decision = new Scanner(System.in);
     System.out.println("Welcome to my Integration project!");
-    /*
-     * A byte is a data type that is composed of 8 bits signed two's complement integer. Min value
-     * is -128 and a maximum of 127 (inclusive). Useful for saving memory in large arrays, also can
-     * be used in place of int. Short data type is 16 bit signed two's complement integer, min value
-     * of -32768, and a maximum of 32767 inclusive. Same guidelines with byte Int is a 32 bit same
-     * stuff above. min value of -2^31, max value of (2^31)-1.In java se 8 and higher the int type
-     * can be used to rep an unsigned 32 bit integer, which has a min value of 0 and max value of
-     * (2^32)-1. Integer class is used to use int data type as an unsigned integer. Static methods
-     * ex: compareUnsigned, divideUnsigned, have been added to the integer class to support math
-     * stuff down da line Long data type is 64 bit, same stuff as above. Signed long has a min value
-     * of -2^63, max value of 2^63)-1. Java se 8 and later, long data type reps unsigned 64 bit
-     * long, min value 0, max value of 2^64)-1, Use the data type when you need a range of values
-     * wider than int's. Long class also contains methods like compareUnsigned, divideUnsigned.
-     * Float data type is a single precision 32 bit IEEE 754 floating point. Very big range of
-     * values, use a float if you need to save large amounts of memory, should never be used for
-     * precise values ex currency. For that use java.math.BigDecimal class. Double data type is a
-     * double precision 64 bit IEEE 754 floating point. Range of values is very big, decimal values,
-     * this data type is used. Never used for currency/precise values. Boolean data type has only
-     * two values: true or false. Used for simple flags that track t/f, only reps one bit of info,
-     * no real size Char data type is single 16 bit unicode character. Min val of '\u0000' or just 0
-     * and a max value of '\uffff' (or 65535 inclusive) Default values: Byte:0 Short:0 Int:0 Long:0L
-     * Float:0.0f Double:0.0d Char:'\u0000' String (any object):null Boolean:false
-     */
+    System.out.println("Would you like to:");
+    System.out.println("1: See some explanations");
+    System.out.println("2: Have some fun");
+    System.out.println("3: Use a calculator");
+    int menuChoice = decision.nextInt();
+    switch (menuChoice) {
+      case 1:
+        example(decision);
+        break;
+      case 2:
+        fun(decision);
+        break;
+
+
+      /*
+       * A byte is a data type that is composed of 8 bits signed two's complement integer. Min value
+       * is -128 and a maximum of 127 (inclusive). Useful for saving memory in large arrays, also
+       * can be used in place of int. Short data type is 16 bit signed two's complement integer, min
+       * value of -32768, and a maximum of 32767 inclusive. Same guidelines with byte Int is a 32
+       * bit same stuff above. min value of -2^31, max value of (2^31)-1.In java se 8 and higher the
+       * int type can be used to rep an unsigned 32 bit integer, which has a min value of 0 and max
+       * value of (2^32)-1. Integer class is used to use int data type as an unsigned integer.
+       * Static methods ex: compareUnsigned, divideUnsigned, have been added to the integer class to
+       * support math stuff down da line Long data type is 64 bit, same stuff as above. Signed long
+       * has a min value of -2^63, max value of 2^63)-1. Java se 8 and later, long data type reps
+       * unsigned 64 bit long, min value 0, max value of 2^64)-1, Use the data type when you need a
+       * range of values wider than int's. Long class also contains methods like compareUnsigned,
+       * divideUnsigned. Float data type is a single precision 32 bit IEEE 754 floating point. Very
+       * big range of values, use a float if you need to save large amounts of memory, should never
+       * be used for precise values ex currency. For that use java.math.BigDecimal class. Double
+       * data type is a double precision 64 bit IEEE 754 floating point. Range of values is very
+       * big, decimal values, this data type is used. Never used for currency/precise values.
+       * Boolean data type has only two values: true or false. Used for simple flags that track t/f,
+       * only reps one bit of info, no real size Char data type is single 16 bit unicode character.
+       * Min val of '\u0000' or just 0 and a max value of '\uffff' (or 65535 inclusive) Default
+       * values: Byte:0 Short:0 Int:0 Long:0L Float:0.0f Double:0.0d Char:'\u0000' String (any
+       * object):null Boolean:false
+       */
+    }
+  }
+
+  public static void example(Scanner decision) {
     boolean boolean1 = true;
     System.out.println("Here's an example of a boolean:" + " " + boolean1);
     int int1 = 105;
@@ -60,41 +80,73 @@ public class Main {
         "Here's \b an example of a \"string\" that has been \\ formatted using \t escape \'sequences\'.";
     // Sorry this is so disgusting to read
     System.out.println(string2);
+  }
+
+
+
+  public static void fun(Scanner decision) {
     System.out.println("What's your favorite color?");
-    Scanner decision = new Scanner(System.in);
+
     String choice = decision.nextLine();
     String finChoice = favColor(choice);
     System.out.println(finChoice);
-  
+
     System.out.println("Would you like some lucky numbers for today ^_^?");
-    
+
     String luckyDecision = decision.nextLine();
     if ((luckyDecision.toLowerCase()).equals("no")) {
-      System.out.println("if you say so....");
-    } else {
+      System.out.println("If you say so....");
+    } else if ((luckyDecision.toLowerCase()).equals("yes")) {
       Random lucky = new Random();
       int number1;
-      for(int i = 1; i<=7;i++) {
-        number1=1+lucky.nextInt(100);
-        while (number1==4) {
-          number1=1+lucky.nextInt(100);
+      for (int i = 1; i <= 7; i++) {
+        number1 = 1 + lucky.nextInt(100);
+        while (number1 == 4) {
+          number1 = 1 + lucky.nextInt(100);
         }
         System.out.println(number1);
       }
+    } else {
+      System.out.println("Invalid, try again");
     }
-    decision.close();
-   
+
+    System.out.println("Welcome to mcdonalds please enter your order number 1-5");
+    int orderNumber = decision.nextInt();
+    switch (orderNumber) {
+      case 1:
+        System.out.println("chincken nungent");
+        break;
+      case 2:
+        System.out.println("hamborger");
+        break;
+      case 3:
+        System.out.println("chebruger");
+        break;
+      case 4:
+        System.out.println("h");
+        break;
+      case 5:
+        System.out.println("sheer heart attack");
+        break;
+      case 69:
+        System.out.println("haha nice");
+        break;
+      default:
+        System.out.println("I don't work at mcdonalds");
+    }
+
   }
 
 
-public static String favColor(String decisionC) {
-  String response1 = "Right Answer!";
-  String response2 = ">:(";
-  if ((decisionC.toLowerCase()).equals("blue")||(decisionC.toLowerCase()).equals("purple")) {
-    return response1;
-  } else {
-    return response2;
-  }
 
-}
+  public static String favColor(String decisionC) {
+    String response1 = "Right Answer!";
+    String response2 = ">:(";
+    if ((decisionC.toLowerCase()).equals("blue") || (decisionC.toLowerCase()).equals("purple")) {
+      return response1;
+    } else {
+      return response2;
+    }
+
+  }
 }
