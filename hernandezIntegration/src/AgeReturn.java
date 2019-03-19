7805fe9fab0ef692d382099b1527250798a9e6dd
dhdzfgcu <43248031+dhdzfgcu@users.noreply.github.com>
2019-03-19 11:56:04 -0400

    heres some barely working code

---
+++ hernandezIntegration/src/AgeReturn.java
@@ -0,0 +1,13 @@
+//Darian Hernandez
+//Class of some examples
+public class AgeReturn {
+private int dayBirth;
+private int monthBirth;
+private int yearBirth;
+public AgeReturn(int day1, int month1, int year1) {
+  dayBirth = setDay(day1);
+  monthBirth = setMonth(month1);
+  yearBirth = setYear(year1);
+}
+public void setYear(int )
+}
