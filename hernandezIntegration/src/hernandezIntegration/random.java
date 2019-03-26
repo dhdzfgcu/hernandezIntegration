c3bf19de28f8f67fa17eb49659ab26407c9d729d
dahernandez0094 <dahernandez0094@H202R1S1.STUDENT.ad.fgcu.edu>
2019-03-26 16:14:48 -0400

    oh godakn

---
+++ hernandezIntegration/src/hernandezIntegration/random.java
@@ -0,0 +1,20 @@
+package hernandezIntegration;
+import java.util.*;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class random {
+    Scanner scan = new Scanner(System.in);
+    int n = scan.nextInt();
+    int d = scan.nextInt();
+    int q = scan.nextInt();
+    int x = scan.nextInt();
+    int y = scan.nextInt();
+    public static void main(String[] args) {
+        int a[][]= new [n][];
+    }
+}
