8ca3af16255eb128be2b2b15af31ea77b129500c
dahernandez0094 <dahernandez0094@H202R1S1.STUDENT.ad.fgcu.edu>
2019-04-02 16:12:04 -0400

    easdgae

--- hernandezIntegration/src/hernandezIntegration/random.java
+++ hernandezIntegration/src/hernandezIntegration/random.java
@@ -6,17 +6,35 @@ import java.util.*;
 import java.text.*;
 import java.math.*;
 import java.util.regex.*;
+import java.util.*;
 
-public class random {
- 
-  public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
-    int n = scan.nextInt();
-    ArrayList<Integer >array1 = new  ArrayList<Integer>(n);
-    
-    
-    
+class MyCalculator {
+  long power(int n, int p) throws Exception {
+  if (n+p==0) {
+    throw new Exception("n and p should not be zero.");
+  }
+  else if(n<0||p<0) {
+    throw new Exception("n or p should not be negative.");
+  } else {
+     return (long) Math.pow(n, p);
   }
 }
-
-
+  
+}
+class Solution {
+  public static final MyCalculator my_calculator = new MyCalculator();
+  public static final Scanner in = new Scanner(System.in);
+  
+  public static void main(String[] args) {
+      while (in .hasNextInt()) {
+          int n = in .nextInt();
+          int p = in .nextInt();
+          
+          try {
+              System.out.println(my_calculator.power(n, p));
+          } catch (Exception e) {
+              System.out.println(e);
+          }
+      }
+  }
+}
